a8a9e9b3d34aab4740b68d51c271d4e46e290e74
Al-amin07 <alsujon2001@gmail.com>
2024-03-02 23:37:24 +0600

    Details section compleate

--- js/index.js
+++ js/index.js
@@ -9,7 +9,14 @@ const loadPost = async () => {
     posts.forEach((post) => {
         // console.log(post);
         const div = document.createElement('div');
-     
+        let addClass = '';
+        if(post.isActive){
+            addClass = 'bg-green-500'
+        }
+        else{
+            addClass = 'bg-red-500';
+        }
+        console.log(addClass)
         
         // if(post.isActive){
         //     active.classList.add('bg-green-500');
@@ -21,7 +28,7 @@ const loadPost = async () => {
         div.innerHTML = `
         <div class="h-[72px] w-[82px] relative">
                 <img class="h-full w-full  rounded-2xl " src="${post.image}" alt="">
-                <div id="active" class="h-3 w-3 absolute -top-1 right-0 rounded-full bg-green-600"></div>
+                <div id="active" class="h-3 w-3 absolute -top-1 right-0 rounded-full ${addClass}"></div>
             </div>
             <div class="">
                 <!-- Author -->
@@ -46,7 +53,7 @@ const loadPost = async () => {
                 </div>
 
                 <div>
-                    <button class="btn"><img src="images/email 1.png" alt=""></button>
+                    <button onclick="clickedEmail('${post.title}','${post.view_count}' )" class="btn"><img src="images/email 1.png" alt=""></button>
                 </div>
                </div>
             </div>
@@ -62,3 +69,19 @@ const loadPost = async () => {
 }
 
 loadPost();
+
+let count = 0;
+const clickedEmail = (title, view) => {
+    count++;
+    document.getElementById('count').innerText = count;
+    const parent = document.getElementById('click-container');
+    const div = document.createElement('div');
+    div.classList = `flex justify-between gap-1 mb-6 bg-white p-4 rounded-xl`
+    div.innerHTML = `
+    <h1 class="font-semibold text-lg">${title}</h1>
+    <h1 class="w-[100px] text-lg"><img class="inline" src="images/eye (1).png" alt=""> <span> ${view
+    }</span></h1>
+    `
+
+    parent.appendChild(div)
+}
